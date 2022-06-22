677b4a307cbf199ad4952b10667d73e3b3a6507c
StringerDM <103386227+StringerDM@users.noreply.github.com>
2022-06-22 09:09:45 +0300

    HW03 main part correction_1

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -28,10 +28,10 @@ CREATE TABLE user_roles
 CREATE TABLE meals
 (
     id              INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
-    date_time       TIMESTAMP             DEFAULT now() NOT NULL,
+    date_time       TIMESTAMP                           NOT NULL,
     description     VARCHAR                             NOT NULL,
     calories        INTEGER                             NOT NULL,
     user_id         INTEGER                             NOT NULL,
-    CONSTRAINT date_time_unique UNIQUE (user_id, date_time),
+    CONSTRAINT meals_unique_user_id_date_time_idx UNIQUE (user_id, date_time),
     FOREIGN KEY (user_id) REFERENCES users (id) ON UPDATE RESTRICT ON DELETE CASCADE
 );
